f1f1e7857efc62672051408e9cb496dee4f66c28
Sushant Sharma <sushantsharma2929@gmail.com>
2025-12-31 02:51:20 +0530

    Update modInt.cpp

--- Templates/modInt.cpp
+++ Templates/modInt.cpp
@@ -1,41 +1,50 @@
 struct mll {
     ll num;
     mll(ll x = 0) { num = x % mod; if (num < 0) num += mod; }
- 
+
     mll& operator+=(mll o){ num += o.num; if (num >= mod) num -= mod; return *this; }
     mll& operator-=(mll o){ num -= o.num; if (num < 0) num += mod; return *this; }
     mll& operator*=(mll o){ num = (ll)((__int128)num * o.num % mod); return *this; }
     mll& operator/=(mll o){ return *this *= o.inv(); }
- 
+
     mll operator+(mll o) const { return mll(*this) += o; }
     mll operator-(mll o) const { return mll(*this) -= o; }
     mll operator*(mll o) const { return mll(*this) *= o; }
     mll operator/(mll o) const { return mll(*this) /= o; }
- 
+
     mll& operator++()  { if (++num == mod) num = 0; return *this; }
     mll& operator--()  { if (num == 0) num = mod; --num; return *this; }
     mll operator++(int){ mll r = *this; ++*this; return r; }
     mll operator--(int){ mll r = *this; --*this; return r; }
- 
+
     static inline mll modexp(mll b, ll e){ mll r = 1; while (e){ if (e & 1) r *= b; b *= b; e >>= 1; } return r; }
     mll pow(ll e) const { return modexp(*this, e); }
     mll inv() const { assert(num != 0); return modexp(*this, mod - 2); }
- 
+
     explicit operator bool() const { return num != 0; }
-    explicit operator ll() const { return num; }
- 
+    explicit operator ll()   const { return num; }
+
     bool operator==(mll o) const { return num == o.num; }
     bool operator!=(mll o) const { return num != o.num; }
     bool operator< (mll o) const { return num <  o.num; }
     bool operator> (mll o) const { return num >  o.num; }
     bool operator<=(mll o) const { return num <= o.num; }
     bool operator>=(mll o) const { return num >= o.num; }
- 
+
+    mll operator%(ll  m) const { return mll(num % m); }
+    mll operator%(int m) const { return mll(num % m); }
+
+    ll operator&(ll  m) const { return num & m; }
+    ll operator&(int m) const { return num & m; }
+    
+    friend ll operator&(ll lhs, const mll& rhs){ return lhs & rhs.num; }
+    friend ll operator&(int lhs, const mll& rhs){ return lhs & rhs.num; }
+
     friend mll operator+(ll lhs, mll rhs){ return mll(lhs) + rhs; }
     friend mll operator-(ll lhs, mll rhs){ return mll(lhs) - rhs; }
     friend mll operator*(ll lhs, mll rhs){ return mll(lhs) * rhs; }
     friend mll operator/(ll lhs, mll rhs){ return mll(lhs) / rhs; }
- 
+
     friend ostream& operator<<(ostream& os, const mll& m){ return os << m.num; }
     friend istream& operator>>(istream& is, mll& m){ ll x; is >> x; m = mll(x); return is; }
 };
